ad02dee6f3cb023e7b7300c819ce52a4db8a80f1
Nick Balsaras <just-nick@Nicks-MacBook-Air.local>
2019-04-19 19:42:24 +1000

    Make errors more verbose

--- src/validation.exception.ts
+++ src/validation.exception.ts
@@ -2,7 +2,7 @@ import { ValidationError } from "./validation-error";
 
 export class ValidationException extends Error {
     constructor(public errors: ValidationError[]){
-        super('Rufus encountered a validation issue');
+        super('Rufus encountered a validation issue: ' + errors.map(e => e.field + ' - ' + e.message + ' - ' + e.value).join(' : '));
         this.name = 'ValidationException';
     }
 }
